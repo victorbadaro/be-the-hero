e60f1dfab1e14735c9d07e489056f165719712ed
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2020-03-29 09:54:22 -0300

    Funcionalidades extras da aplicação

---
+++ backend/tests/integration/ong.spec.js
@@ -0,0 +1,31 @@
+const request = require('supertest');
+const app = require('../../src/app');
+const connection = require('../../src/database/connection');
+
+describe('ONG', () => {
+    beforeEach(async () => {
+        await connection.migrate.latest();
+    });
+
+    afterAll(async () => {
+        await connection.migrate.rollback();
+        await connection.destroy();
+    });
+
+    it('should be able to create a new ONG', async () => {
+        const response = await request(app)
+            .post('/ongs')
+            // caso precise enviar um header (no caso abaixo, o Authorization e o ID de uma ONG válida):
+            // .set('Authorization', 'c0672a0e')
+            .send({
+                name: "APAD2",
+                email: "contato@teste.com",
+                whatsapp: "4700000000",
+                city: "Rio do Sul",
+                uf: "SC"
+            });
+        
+        expect(response.body).toHaveProperty('id');
+        expect(response.body.id).toHaveLength(8);
+    });
+});
